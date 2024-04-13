202847bcd3ebefbd97665da52d97a5a6d1d49280
Alex Aellen <aaellen01@gmail.com>
2024-04-13 21:19:11 +0100

    Add scrolling div for planet info

--- static/space.js
+++ static/space.js
@@ -2,15 +2,15 @@
 
 var totalScroll = 0;
 var zoomThresholds = [[0, 1], [1300, 1], [1500, 3], [1700, 1],
-                      [3300, 1], [3500, 2], [3700, 1],
-                      [5300, 1], [5500, 0.5], [5700, 1]];
+[3300, 1], [3500, 2], [3700, 1],
+[5300, 1], [5500, 0.5], [5700, 1]];
+
+const MAX_SCROLL = 7000;
 
 window.onload = function() {
 
 
   let scrollSize = 0.5;
-
-
 
 
   var parent = document.getElementById("canvas");
@@ -40,19 +40,19 @@ window.onload = function() {
     // transform point
     const transform = ctx.getTransform();
     const x = event.offsetX - transform.e;
-    const y = event.offsetY - transform.f - ctx.canvas.height/2;
+    const y = event.offsetY - transform.f - ctx.canvas.height / 2;
 
-    console.log(x,y)
+    console.log(x, y)
     let zoom = getCurrentZoom();
 
     // find which object was clicked
     let any = false;
     objects.forEach(obj => {
       // if the x,y is in this object;
-      if (obj.x - obj.width*zoom/2 <= x &&
-        obj.y - obj.height*zoom/2 <= y &&
-        x <= obj.x + obj.width*zoom/2 &&
-        y <= obj.y + obj.height*zoom/2) {
+      if (obj.x - obj.width * zoom / 2 <= x &&
+        obj.y - obj.height * zoom / 2 <= y &&
+        x <= obj.x + obj.width * zoom / 2 &&
+        y <= obj.y + obj.height * zoom / 2) {
         onClick(obj, ctx);
         any = true;
         // dont worry about multiple objects being clicked at the same time
@@ -60,12 +60,12 @@ window.onload = function() {
       }
     });
 
-    if (any){
+    if (any) {
       drawObjects(ctx, objects);
     }
   })
 
-  canvas.addEventListener("wheel", (event) => {
+  document.addEventListener("wheel", (event) => {
     event.preventDefault();
 
     let dx = -event.wheelDeltaY * scrollSize;
@@ -75,7 +75,32 @@ window.onload = function() {
 
 
 
-    
+    objects.forEach(obj => {
+      let left = parseInt(obj.div.style.left);
+
+      left += dx;
+      obj.div.style.left = left + "px";
+
+
+      if (left < -obj.div.offsetWidth || left > ctx.canvas.width) {
+        // outside the screen
+        obj.div.style.display = "none";
+      } else {
+        //fade in out
+
+        obj.div.style.display = "block";
+
+        let t = (left + obj.div.offsetWidth / 2) / ctx.canvas.width;
+        // quadratic with intercepts at 0 and 1 peak at 0.5,1
+        obj.div.style.opacity = -4 * t * (t - 1);
+
+
+      }
+
+
+    })
+
+
 
 
     // clear the canvas to re-draw
@@ -92,7 +117,7 @@ window.onload = function() {
   });
 
 
-  window.onresize = function(event){
+  window.onresize = function(event) {
     console.log("resizing");
     ctx.save();
     ctx.canvas.width = window.innerWidth;
@@ -105,31 +130,28 @@ window.onload = function() {
 
 }
 
-function getCurrentZoom(){
+function getCurrentZoom() {
 
 
-  for(let i=1; i<zoomThresholds.length; i++){
-    if (totalScroll < zoomThresholds[i][0]){
+  for (let i = 1; i < zoomThresholds.length; i++) {
+    if (totalScroll < zoomThresholds[i][0]) {
 
-      let start = zoomThresholds[i-1][1];
+      let start = zoomThresholds[i - 1][1];
       let end = zoomThresholds[i][1];
 
-      let t = (totalScroll - zoomThresholds[i-1][0])/(zoomThresholds[i][0] - zoomThresholds[i-1][0]);
+      let t = (totalScroll - zoomThresholds[i - 1][0]) / (zoomThresholds[i][0] - zoomThresholds[i - 1][0]);
 
-      console.log(t);
-      t = Math.min(1,t);
-      t = Math.max(0,t);
-      console.log(t);
+      t = Math.min(1, t);
+      t = Math.max(0, t);
 
-      console.log(totalScroll, i, start, end, t);
-      return start + (end-start) *t;
+      return start + (end - start) * t;
 
     }
   }
 
-    // bigger than all thresholds
-    // shoudl we limit the scroll??
-  return zoomThresholds[zoomThresholds.length-1][1];
+  // bigger than all thresholds
+  // shoudl we limit the scroll??
+  return zoomThresholds[zoomThresholds.length - 1][1];
 
 
 }
@@ -153,7 +175,7 @@ function makeObject(name, src, x, scale = 1) {
     obj.y = 0; //(HEIGHT- obj.height) / 2;
 
     // make all (almost) white pixels transparent
-    const THRESHOLD = 240;
+    const THRESHOLD = 230;
 
     var tempCanvas = document.createElement("canvas");
     tempCanvas.width = image.width;
@@ -177,25 +199,39 @@ function makeObject(name, src, x, scale = 1) {
     let img = new Image();
     img.src = tempCanvas.toDataURL();
     obj.img = img;
+
+
+    img.onload = function() {
+      // create the div for the info
+      let div = document.createElement("div");
+      div.className = "info-planet";
+      div.id = "info-" + name;
+      div.style.left = (obj.x + img.width) + "px";
+      div.style.top = "100px";
+      document.body.appendChild(div);
+      obj.div = div;
+    }
+
+
   }
 
   return obj;
 }
 
 
-function clear(ctx){
+function clear(ctx) {
 
-    // Store the current transformation matrix
-    ctx.save();
+  // Store the current transformation matrix
+  ctx.save();
 
-    // Use the identity matrix while clearing the canvas
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  // Use the identity matrix while clearing the canvas
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-    // ctx.fillStyle = "black"; // space is black
-    // ctx.fillRect(0, 0, canvas.width, canvas.height);
-    // Restore the transform
-    ctx.restore();
+  // ctx.fillStyle = "black"; // space is black
+  // ctx.fillRect(0, 0, canvas.width, canvas.height);
+  // Restore the transform
+  ctx.restore();
 }
 
 function drawObjects(ctx, objects) {
@@ -206,9 +242,9 @@ function drawObjects(ctx, objects) {
 
   objects.forEach(obj => {
     // console.log(obj.img)
-    let x = obj.x - obj.width*zoom/2;
-    let y = obj.y - obj.height*zoom/2 + ctx.canvas.height/2 ;
-    ctx.drawImage(obj.img, x,y, obj.width * zoom, obj.height * zoom); //obj.img.width, obj.img.height);
+    let x = obj.x - obj.width * zoom / 2;
+    let y = obj.y - obj.height * zoom / 2 + ctx.canvas.height / 2;
+    ctx.drawImage(obj.img, x, y, obj.width * zoom, obj.height * zoom); //obj.img.width, obj.img.height);
   })
 
 }
@@ -219,57 +255,6 @@ function onClick(object, ctx) {
 
 
   console.log(object);
-  //
-  //
-  //
-  // // set scale so that the object fills the screen
-  // //
-  // let ratioX = ctx.canvas.width / object.width;
-  // let ratioY = ctx.canvas.height / object.height;
-  //
-  // let ratio = Math.max(ratioX, ratioY);
-  //
-  // // let tx = object.x;
-  //
-  //
-  // // translate by -current - center
-  //
-  //
-  //   
-  //
-  // // let ty = object.y;
-  //
-  // // how do we do this smoothly??
-  // // identity martix
-  // let currentTransform = ctx.getTransform();
-  // let cx = object.width/2; 
-  // let cy = object.height/2;
-  //
-  // let tx  = cx + (ctx.canvas.width - object.width*ratio)/2;
-  // let ty  = cy + (ctx.canvas.height - object.height*ratio)/2;
-  //
-  //
-  //
-  // // new matrix for transform of tx,ty then scale of s is:
-  // // s 0 tx
-  // // 0 s ty
-  // // 0 0 1
-  // let M1 = new DOMMatrix([ratio, 0, 0, ratio, cx, -cy]); // translate and scale about origin
-  // let M2 = new DOMMatrix([1, 0, 0, 1, tx, ty]); // move to right place
-  // 
-  // console.log(M1);
-  // console.log(M2);
-  //
-  // // let newTransform = new DOMMatrix([ratio, 0, 0, ratio, tx, ty]);
-  //
-  //
-  // ctx.setTransform(M1); 
-  // ctx.translate(tx, ty);
-  //
-  //
-  // console.log(currentTransform, ctx.getTransform());
-
-
-
 }
+
 
