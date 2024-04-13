97f7f29b2cb993e80267cefd55ffc1ea3835dd7b
Max <maximilian.bolt@student.manchester.ac.uk>
2024-04-13 18:51:25 +0100

    works initally

--- UniServerZ/www/AiText.py
+++ UniServerZ/www/AiText.py
@@ -1,17 +1,48 @@
-import vertexai
-from vertexai.language_models import TextGenerationModel
-def handleInput(text):
-    
-    # needed imports:
-    #to install below:   pip install --upgrade google-cloud-aiplatform
+"""
+At the command line, only need to run once to install the package via pip:
 
-    #process Ai here
+$ pip install google-generativeai
+"""
 
-    temperature = 0.9
-    project_id = 00000000000000000
-    location = "deez"
+import google.generativeai as genai
 
+genai.configure(api_key="AIzaSyDmRJ6AAzEhXe4tomtQOG7PzzqCP4Vsxhs")
 
+# Set up the model
+generation_config = {
+  "temperature": 0.9,
+  "top_p": 1,
+  "top_k": 1,
+  "max_output_tokens": 2048,
+}
 
-    textOut = "tmp"
-    return textOut
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_LOW_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_LOW_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_LOW_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_LOW_AND_ABOVE"
+  },
+]
+
+model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
+
+convo = model.start_chat(history=[
+])
+
+#call this to get inputs
+convo.send_message("Your general instruction: <You are an alien called Sam who lives on Mars, and will tell the user about space facts relating to mars. If the user talks about other topics, guide the conversation back to your home planet. Provide short to medium length answers aimed at medium age children.> Answer the user message: <Hi! Tell me fact about mars>")
+#convo.send_message("what about rivers")
+print(convo.last.text)
