de538bd9f2711ef3981d73710192356babfc6a3a
Max <maximilian.bolt@student.manchester.ac.uk>
2024-04-13 12:02:34 +0100

    basic homepage

---
+++ UniServerZ/www/homepage.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:RebeccaPurple;">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <title>Homepage</title>
+    <h1 style="color:MistyRose;"> Cat Gang Cat Gang </h1>
+
+
+</head>
+
+<body>
+    <p style="color:MistyRose;"> In space nobody can hear you scream </p>
+
+</body>
