4404192185595389971e98793fcdfa5587f9ed27
Abhinav Akkena <abhinav.akkena@student.manchester.ac.uk>
2024-04-13 19:45:24 +0100

    Add basic AI route

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import AiText
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/chatbot/earth", methods=["POST"])
+def chatbot_earth():
+    # Probably should do input sanitisation and error handling
+    user_input = request.form['user_input']
+    response = {"response": AiText.getResponse(user_input)}
+    return response
+
+
+
+with app.app_context():
+    # Start AI conversations
+    AiText.getResponse("")
