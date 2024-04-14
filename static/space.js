8aa157fdce186553c878b790c46fd9033cde31ad
Alex Aellen <aaellen01@gmail.com>
2024-04-14 09:07:35 +0100

    Move divs to the left

--- static/space.js
+++ static/space.js
@@ -21,51 +21,19 @@ window.onload = function() {
   var ctx = canvas.getContext('2d');
 
 
-  let width = ctx.canvas.width / 2;
+  let width = ctx.canvas.width / 3;
   // make sure these are in order
-  makeObject("sun", -6000, 1.3, 0.23  , 0,0,0 ,0, 0) //333,000
-  makeObject("mercury", -4000, 1.8, 0.05, 0,0,0 ,0, 0) //0.055
-  makeObject("venus", -2000, 1.6, 0.08, 0,0,0 ,0, 0) //0.8
-  makeObject("earth", 0, 1.4, 0.1, 0,0,0 ,0, 0); 
-  makeObject("moon", 2000, 1.75, 0.05, 0,0,0 ,0, 0); //0.25
-  makeObject("mars", 4000, 1.5, 0.08, 0,0,0 ,0, 0); //0.1
-  makeObject("jupiter", 6000, 1.5, 0.16, 0.35,-1200,-200 ,0, 0); //11
-  makeObject("saturn", 8000, 1.4, 0.18, 0,0,0 ,0, 0); //95
-  makeObject("uranus", 10000, 1.4, 0.1, 0,0,0 ,0, 0); //14.5
-  makeObject("neptune", 12000, 1.5, 0.09, 0,0,0 ,0, 0); //17
-  makeObject("pluto", 14000, 1.2, 0.06, 0,0,0 ,0, 0); //0.2
-  
-  // const rels = [[0,0], [0,0], [0,0], [0,0]];
-  //
-  // let i=0;
-  // planets.forEach(obj=>{
-  //
-  //   let div = document.createElement("div");
-  //   div.className = "info-planet";
-  //   div.id = "info-"+obj.name;
-  //   div.style.opacity = 0; //Max: so doesnt appear at start
-  //   div.innerHTML = `<div class="chatbox">
-  //           <div class="messages" id="messageBox_${obj.name}">
-  //
-  //           </div>
-  //           <br>
-  //           <div class="user_input">
-  //               <form action="javascript:;" onsubmit="handleSubmit('${obj.name}')">
-  //                   <input type="text" id="user_input_${obj.name}" placeholder = "Ask me anything..." autocomplete="off" name="user_input"><br>
-  //               </form>
-  //           </div>
-  //       </div>
-  //       `;
-  //     document.body.appendChild(div);
-  //   // let div = document.getElementById("info-"+obj.name);
-  //   // what if there is no div?
-  //
-  //   div.style.left = (obj.x + obj.divRelLeft) +"px";
-  //   div.style.top = (125 + obj.divRelTop) + "px";
-  //   obj.div = div;
-  //
-  //   i+=1;
-  // })
+  makeObject("sun", -6000, 1.3, 0.23, 0, 0, 0, -width, 0) //333,000
+  makeObject("mercury", -4000, 1.8, 0.05, 0, 0, 0, -width, 0) //0.055
+  makeObject("venus", -2000, 1.6, 0.08, 0, 0, 0, -width, 0) //0.8
+  makeObject("earth", 0, 1.4, 0.1, 0, 0, 0, -width, 0);
+  makeObject("moon", 2000, 1.75, 0.05, 0, 0, 0, -width, 0); //0.25
+  makeObject("mars", 4000, 1.5, 0.08, 0, 0, 0, -width, 0); //0.1
+  makeObject("jupiter", 6000, 1.5, 0.16, 0.35, -1200, -200, -width, 0); //11
+  makeObject("saturn", 8000, 1.4, 0.18, 0, 0, 0, -width, 0); //95
+  makeObject("uranus", 10000, 1.4, 0.1, 0, 0, 0 - width, 0); //14.5
+  makeObject("neptune", 12000, 1.5, 0.09, 0, 0, 0, -width, 0); //17
+  makeObject("pluto", 14000, 1.2, 0.06, 0, 0, 0, -width, 0); //0.2
 
   setTimeout(() => {
     drawPlanets(ctx);
@@ -74,27 +42,27 @@ window.onload = function() {
 
   canvas.addEventListener("click", event => {
 
-    // transform point
-    const transform = ctx.getTransform();
-    const x = event.offsetX - transform.e;
-    const y = event.offsetY - transform.f - ctx.canvas.height / 2;
-
-    let zoom = getCurrentZoom();
-
-    // find which object was clicked
-    let any = false;
-    planets.forEach(obj => {
-      // if the x,y is in this object;
-      if (obj.x - obj.width * zoom / 2 <= x &&
-        obj.y - obj.height * zoom / 2 <= y &&
-        x <= obj.x + obj.width * zoom / 2 &&
-        y <= obj.y + obj.height * zoom / 2) {
-        onClick(obj, ctx);
-        any = true;
-        // dont worry about multiple objects being clicked at the same time
-        // because they should be far enough apart
-      }
-    });
+    // // transform point
+    // const transform = ctx.getTransform();
+    // const x = event.offsetX - transform.e;
+    // const y = event.offsetY - transform.f - ctx.canvas.height / 2;
+    //
+    // let zoom = getCurrentZoom();
+    //
+    // // find which object was clicked
+    // let any = false;
+    // planets.forEach(obj => {
+    //   // if the x,y is in this object;
+    //   if (obj.x - obj.width * zoom / 2 <= x &&
+    //     obj.y - obj.height * zoom / 2 <= y &&
+    //     x <= obj.x + obj.width * zoom / 2 &&
+    //     y <= obj.y + obj.height * zoom / 2) {
+    //     onClick(obj, ctx);
+    //     any = true;
+    //     // dont worry about multiple objects being clicked at the same time
+    //     // because they should be far enough apart
+    //   }
+    // });
   })
 
   document.addEventListener("wheel", (event) => {
@@ -121,11 +89,11 @@ window.onload = function() {
     drawPlanets(ctx);
   };
 
-  
+
   //Max: avoid visual bug at start by redrawing after load
-  clear(ctx); 
-  
-  
+  clear(ctx);
+
+
 }
 
 function getCurrentZooms(ctx) {
@@ -134,7 +102,7 @@ function getCurrentZooms(ctx) {
   let ts = [];
 
   let boundSize = ctx.canvas.width / 3;
-  let middle = ctx.canvas.width *3/ 4; // middle of slow section in window 
+  let middle = ctx.canvas.width * 3 / 4; // middle of slow section in window 
   let leftBound = middle - boundSize;
   let rightBound = middle + boundSize;
 
@@ -172,9 +140,9 @@ function getCurrentZooms(ctx) {
   return [zooms, ts];
 }
 
-function makeObject(name, x, zoom = 1, scale = 1, alien_scale=1, alienRelX, alienRelY, divRelLeft, divRelTop) {
-  scale = scale*1.6;
-  zoom = zoom*1.6;
+function makeObject(name, x, zoom = 1, scale = 1, alien_scale = 1, alienRelX, alienRelY, divRelLeft, divRelTop) {
+  scale = scale * 1.6;
+  zoom = zoom * 1.6;
   let obj = {
     name: name,
     x: x,
@@ -187,33 +155,33 @@ function makeObject(name, x, zoom = 1, scale = 1, alien_scale=1, alienRelX, alie
   }
   let src = `static/${name}.png`;
   let alien_src = `static/${name}_alien.png`;
-  
+
   let div = document.createElement("div");
-    div.className = "info-planet";
-    div.id = "info-"+obj.name;
-    div.style.opacity = 0; //Max: so doesnt appear at start
-    div.innerHTML = `<div class="chatbox">
+  div.className = "info-planet";
+  div.id = "info-" + obj.name;
+  div.style.opacity = 0; //Max: so doesnt appear at start
+  div.innerHTML = `<div class="chatbox">
             <div class="messages" id="messageBox_${obj.name}">
 
             </div>
 
             <div class="user_input">
                 <form action="javascript:;" onsubmit="handleSubmit('${obj.name}')">
-                    <input type="text" id="user_input_${obj.name}" name="user_input"><br>
+            <input type="text" id="userinput${obj.name}" placeholder = "Ask me anything..." autocomplete="off" name="user_input"><br>
                 </form>
             </div>
         </div>
         `;
 
-      document.body.appendChild(div);
+  document.body.appendChild(div);
 
-    obj.relLeft = divRelLeft; //(obj.x - div.offsetWidth/2 + divRelLeft) +"px";
-    
-    // let left = obj.x + ctx.getTransform().e;
-    // obj.div.style.left = left + "px";
-    div.style.top = (125 + divRelTop) + "px";
+  obj.relLeft = divRelLeft; //(obj.x - div.offsetWidth/2 + divRelLeft) +"px";
 
-    obj.div = div;
+  // let left = obj.x + ctx.getTransform().e;
+  // obj.div.style.left = left + "px";
+  div.style.top = (125 + divRelTop) + "px";
+
+  obj.div = div;
 
 
   // add the planet image
@@ -272,7 +240,7 @@ function clear(ctx) {
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
   //Max: space colour background, could replace with image?
   ctx.fillStyle = '#1A102A';
-  ctx.fillRect(0,0, ctx.canvas.width, ctx.canvas.height);
+  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
   // ctx.fillStyle = "black"; // space is black
   // ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -286,18 +254,18 @@ function drawPlanets(ctx) {
   for (let i = 0; i < planets.length; i += 1) {
     let obj = planets[i];
     let zoom = zooms[i];
-    let w = obj.width*zoom;
-    let h = obj.height*zoom;
+    let w = obj.width * zoom;
+    let h = obj.height * zoom;
     let x = obj.x;
-    let y = obj.y + ctx.canvas.height/2; 
-    ctx.drawImage(obj.img, x-w/2, y-h/2, w, h); //obj.img.width, obj.img.height);
-    ctx.drawImage(obj.alien_img, x+obj.alienX, y+obj.alienY, obj.alien_width, obj.alien_height); //obj.img.width, obj.img.height);
+    let y = obj.y + ctx.canvas.height / 2;
+    ctx.drawImage(obj.img, x - w / 2, y - h / 2, w, h); //obj.img.width, obj.img.height);
+    ctx.drawImage(obj.alien_img, x + obj.alienX, y + obj.alienY, obj.alien_width, obj.alien_height); //obj.img.width, obj.img.height);
 
     // update divs
     // let left = parseInt(obj.div.style.left);
     // left += dx;
 
-    let left = obj.x + ctx.getTransform().e - w/2 + obj.relLeft; // - obj.img.width/2 - obj.div.offsetWidth/2;
+    let left = obj.x + ctx.getTransform().e - w / 2 + obj.relLeft; // - obj.img.width/2 - obj.div.offsetWidth/2;
     obj.div.style.left = left + "px";
 
     if (ts[i] == null) {
@@ -308,15 +276,15 @@ function drawPlanets(ctx) {
 
       obj.div.style.display = "block";
       let t = ts[i];
-      
+
       // Max: added so is at full opacity for a range not a point- easier use
-      if(t< 0.5) {
+      if (t < 0.5) {
         obj.div.style.opacity = 1
       } else {
-        obj.div.style.opacity = 2 - 2*t;
+        obj.div.style.opacity = 2 - 2 * t;
       }
 
-      if(t<0.2) {
+      if (t < 0.2) {
         scrollSize = 0.06;
       }
       else {
